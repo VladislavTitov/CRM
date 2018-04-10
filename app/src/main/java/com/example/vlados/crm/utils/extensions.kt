26042f96709cbca9b69f477eca63a1558a0275f0
VladislavTitov <titovvlad1997@yandex.ru>
2018-04-10 21:29:34 +0300

    added default properties ext fun

--- app/src/main/java/com/example/vlados/crm/utils/extensions.kt
+++ app/src/main/java/com/example/vlados/crm/utils/extensions.kt
@@ -1,8 +1,11 @@
 package com.example.vlados.crm.utils
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.text.Editable
 import android.text.TextWatcher
 import android.widget.EditText
+
 
 /**
  * Created by vlados on 26.03.18.
@@ -20,3 +23,12 @@ fun EditText.afterTextChanged(afterTextChanged: (String) -> Unit) {
         }
     })
 }
+
+fun Context.getApplicationName(): String {
+    val applicationInfo = this.applicationInfo
+    val stringId = applicationInfo.labelRes
+    return if (stringId == 0) applicationInfo.nonLocalizedLabel.toString() else this.getString(stringId)
+}
+
+val Context.defaultProperties: SharedPreferences
+    get() = this.getSharedPreferences(this.getApplicationName(), Context.MODE_PRIVATE)
