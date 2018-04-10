9ba2f30c41048b2b5e00e4a0ac211e638edf4321
VladislavTitov <titovvlad1997@yandex.ru>
2018-04-10 18:26:15 +0300

    added utils

---
+++ app/src/main/java/com/example/vlados/crm/utils/extensions.kt
@@ -0,0 +1,22 @@
+package com.example.vlados.crm.utils
+
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+
+/**
+ * Created by vlados on 26.03.18.
+ */
+fun EditText.afterTextChanged(afterTextChanged: (String) -> Unit) {
+    this.addTextChangedListener(object : TextWatcher {
+        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        }
+
+        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        }
+
+        override fun afterTextChanged(editable: Editable?) {
+            afterTextChanged.invoke(editable.toString())
+        }
+    })
+}
