8ef06b6a0438425727c22c808cf4668ed695f014
VladislavTitov <titovvlad1997@yandex.ru>
2018-04-24 01:39:54 +0300

    added roles mapping

--- app/src/main/java/com/example/vlados/crm/Roles.kt
+++ app/src/main/java/com/example/vlados/crm/Roles.kt
@@ -5,3 +5,9 @@ const val CASHIER = "cashier"
 const val SHOP_ADMIN = "shop_admin"
 
 const val NET_ADMIN = "net_admin"
+
+val mapToTitleId = mapOf(
+        CASHIER to R.string.cashier,
+        SHOP_ADMIN to R.string.shop_admin,
+        NET_ADMIN to R.string.net_admin
+)
