9773f5ca7041f1b4497b7c8e829150dd7ca02ebb
VladislavTitov <titovvlad1997@yandex.ru>
2018-05-10 04:05:40 +0300

    added getAllGoods method

--- app/src/main/java/com/example/vlados/crm/network/GetMethods.kt
+++ app/src/main/java/com/example/vlados/crm/network/GetMethods.kt
@@ -1,4 +1,12 @@
 package com.example.vlados.crm.network
 
+import com.example.vlados.crm.db.models.Good
+import io.reactivex.Single
+import retrofit2.http.GET
+
 interface GetMethods {
+
+    @GET("goods")
+    fun getAllGoods() : Single<List<Good>>
+
 }
