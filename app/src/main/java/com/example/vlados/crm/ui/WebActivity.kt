553cb4cf04e912175cb64a55f031d7c25df1561e
Daria Popova <neoneo111111@gmail.com>
2018-05-16 18:36:59 +0300

    share report url

--- app/src/main/java/com/example/vlados/crm/ui/WebActivity.kt
+++ app/src/main/java/com/example/vlados/crm/ui/WebActivity.kt
@@ -3,14 +3,14 @@ package com.example.vlados.crm.ui
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.webkit.WebChromeClient
-import com.example.vlados.crm.R
-import kotlinx.android.synthetic.main.activity_web.*
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 import android.webkit.WebView
 import android.webkit.WebViewClient
-
+import com.example.vlados.crm.R
+import kotlinx.android.synthetic.main.activity_web.*
 
 
 fun Context.getWebActivityIntent(url: String): Intent {
@@ -19,18 +19,45 @@ fun Context.getWebActivityIntent(url: String): Intent {
 }
 
 class WebActivity : AppCompatActivity() {
-
+    
+    var url: String? = null
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web)
-
+  
+        
         reports_view.getSettings().setJavaScriptEnabled(true);
         reports_view.webViewClient = MyWebViewClient()
-
-        val url = intent.data
+        url = intent.data.toString()
         reports_view.loadUrl(url.toString())
+        
     }
-
+    
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.reposts_menu, menu)
+        return true
+    }
+    
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.shareReport -> {
+                share(url)
+                return true
+            }
+            else -> return false
+        }
+        
+    }
+    
+    private fun share(url: String?) {
+        val sendIntent = Intent()
+        sendIntent.action = Intent.ACTION_SEND
+        sendIntent.putExtra(Intent.EXTRA_TEXT, url)
+        sendIntent.type = "text/plain"
+        startActivity(sendIntent)
+    }
+    
     private inner class MyWebViewClient : WebViewClient() {
         override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
             view.loadUrl(url)
